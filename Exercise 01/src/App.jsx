575c94b3b5383c460c490522ff45cfed484d560c
abdsaed012 <abdalesaid115@gmail.com>
2025-11-09 17:32:45 -0800

    Exercise 01

---
+++ Exercise 01/src/App.jsx
@@ -0,0 +1,12 @@
+import UserCard from "./Usecard"
+
+const App = ()=>{
+  return(
+  <>
+  <UserCard/>
+  </>
+  )
+  
+}
+
+export default App;
---
+++ Exercise 01/src/Usecard.jsx
@@ -0,0 +1,10 @@
+const UserCard = () => {
+  return (
+    <div>
+      <h2>abdulahi saed</h2>
+      <p>abdasaed@gmail.com</p>
+    </div>
+  );
+};
+
+export default UserCard;
